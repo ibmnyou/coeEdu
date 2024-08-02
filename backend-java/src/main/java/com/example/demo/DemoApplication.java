d332bd96e6900242e6fab61fca306034d907fa1e
JoohoYoon <67041798+ibmnyou@users.noreply.github.com>
2024-08-02 14:01:00 +0900

    Update DemoApplication.java

--- backend-java/src/main/java/com/example/demo/DemoApplication.java
+++ backend-java/src/main/java/com/example/demo/DemoApplication.java
@@ -13,6 +13,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
 	@GetMapping(value = "/")
 	public String doGetHelloWorld() {
 		return "Hello World";
